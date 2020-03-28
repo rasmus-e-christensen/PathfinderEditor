0f16280b157e60ef1022a7d92e38f752b6006217
Simbas Desktop <rech16@student.aau.dk>
2020-03-28 20:16:44 +0100

    added workign ancestry

--- app/static/js/ancestry.js
+++ app/static/js/ancestry.js
@@ -1,32 +1,33 @@
-$('#ancestry_select').change(function(){
+function changeAncestry(){
+   var sel_ancestry = document.getElementById('ancestry_select').value;
    var ancestry = document.getElementById("chosen_ancestry");
-   var json = $.getJSON("/ancestry_data");
-   switch (this.value) {
+   var obj = {{ data | tojson | safe }};
+   switch (sel_ancestry) {
   case "Dwarf":
-    ancestry.html(json);
+    ancestry.innerHTML = "You choose " + obj[0].name;
     break;
   case "Elf":
-    ancestry.html(json)
+    document.getElementById("chosen_ancestry").innerHTML = "You choose " + sel_ancestry;
     break;
   case "Gnome":
-    document.getElementById("chosen_ancestry").innerHTML = "You choose " + this.value;
+    document.getElementById("chosen_ancestry").innerHTML = "You choose " + sel_ancestry;
     break;
   case "Goblin":
-    document.getElementById("chosen_ancestry").innerHTML = "You choose " + this.value;
+    document.getElementById("chosen_ancestry").innerHTML = "You choose " + sel_ancestry;
     break;
   case "Halfling":
-    document.getElementById("chosen_ancestry").innerHTML = "You choose " + this.value;
+    document.getElementById("chosen_ancestry").innerHTML = "You choose " + sel_ancestry;
     break;
   case "Human":
-    document.getElementById("chosen_ancestry").innerHTML = "You choose " + this.value;
+    document.getElementById("chosen_ancestry").innerHTML = "You choose " + sel_ancestry;
     break;
   case "Hobgoblin":
-    document.getElementById("chosen_ancestry").innerHTML = "You choose " + this.value;
+    document.getElementById("chosen_ancestry").innerHTML = "You choose " + sel_ancestry;
     break;
   case "Leshy":
-    document.getElementById("chosen_ancestry").innerHTML = "You choose " + this.value;
+    document.getElementById("chosen_ancestry").innerHTML = "You choose " + sel_ancestry;
     break;
   case "Lizardfolk":
-    document.getElementById("chosen_ancestry").innerHTML = "You choose " + this.value;
+    document.getElementById("chosen_ancestry").innerHTML = "You choose " + sel_ancestry;
     break;
-}});
+}};
