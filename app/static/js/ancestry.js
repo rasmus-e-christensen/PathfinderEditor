8353258b8e76be281f2146ab6fc1ebb7c38bf551
Simbas Desktop <rech16@student.aau.dk>
2020-03-30 16:15:12 +0200

    refactored a bit the ancestry functions

--- app/static/js/ancestry.js
+++ app/static/js/ancestry.js
@@ -15,50 +15,68 @@ function resetAncestryRowAbilityScores() {
     document.getElementById("cha_ances_mod").innerHTML = 0;
 };
 
-function dwarf(){
-    document.getElementById('con_ances_mod').innerHTML = 2;
-    document.getElementById('wis_ances_mod').innerHTML = 2;
-    document.getElementById('cha_ances_mod').innerHTML = -2;
+
+function ability_flaw_boost(array){
+    resetAncestryRowAbilityScores();
+    for (i = 0; i < 2; i++) {
+        document.getElementById(array[i].concat('_ances_mod')).innerHTML = 2;
+    }
+    for (i = 2; i < 3; i++) {
+        document.getElementById(array[i].concat('_ances_mod')).innerHTML = -2;
+    }
 };
 
-function elf(){
-    document.getElementById('dex_ances_mod').innerHTML = 2;
-    document.getElementById('int_ances_mod').innerHTML = 2;
-    document.getElementById('con_ances_mod').innerHTML = -2;
-};
 
-function gnome(){
-    document.getElementById('con_ances_mod').innerHTML = 2;
-    document.getElementById('cha_ances_mod').innerHTML = 2;
-    document.getElementById('str_ances_mod').innerHTML = -2;
-};
 
-function goblin(){
-    document.getElementById('dex_ances_mod').innerHTML = 2;
-    document.getElementById('cha_ances_mod').innerHTML = 2;
-    document.getElementById('wis_ances_mod').innerHTML = -2;
-};
-
-function halfling(){
-    document.getElementById('dex_ances_mod').innerHTML = 2;
-    document.getElementById('wis_ances_mod').innerHTML = 2;
-    document.getElementById('str_ances_mod').innerHTML = -2;
-};
-
-function hobgoblin(){
-    document.getElementById('con_ances_mod').innerHTML = 2;
-    document.getElementById('int_ances_mod').innerHTML = 2;
-    document.getElementById('wis_ances_mod').innerHTML = -2;
-};
-
-function leshy(){
-    document.getElementById('con_ances_mod').innerHTML = 2;
-    document.getElementById('wis_ances_mod').innerHTML = 2;
-    document.getElementById('int_ances_mod').innerHTML = -2;
-};
-
-function lizardfolk(){
-    document.getElementById('str_ances_mod').innerHTML = 2;
-    document.getElementById('wis_ances_mod').innerHTML = 2;
-    document.getElementById('int_ances_mod').innerHTML = -2;
+function switchAncestry(sel_ancestry, obj , index){
+    switch (sel_ancestry) {
+    case "Dwarf":
+        changeAncestryTable(obj, index);
+        ability_flaw_boost(['con', 'wis', 'cha']);
+        break;
+    case "Elf":
+        changeAncestryTable(obj, index);
+        ability_flaw_boost(['dex', 'int', 'con']);
+        break;
+    case "Gnome":
+        changeAncestryTable(obj, index);
+        ability_flaw_boost(['con', 'cha', 'str']);
+        break;
+    case "Goblin":
+        changeAncestryTable(obj, index);
+        ability_flaw_boost(['dex', 'cha', 'wis']);
+        break;
+    case "Halfling":
+        changeAncestryTable(obj, index);
+        ability_flaw_boost(['dex', 'wis', 'str']);
+        break;
+    case "Half-Elf":
+        changeAncestryTable(obj, index);
+        break;
+    case "Half-Orc":
+        changeAncestryTable(obj, index);
+        break;
+    case "Human":
+        changeAncestryTable(obj, index);
+        break;
+    case "Hobgoblin":
+        changeAncestryTable(obj, index);
+        ability_flaw_boost(['con', 'int', 'wis']);
+        break;
+    case "Leshy":
+        changeAncestryTable(obj, index);
+        ability_flaw_boost(['con', 'wis', 'int']);
+        break;
+    case "Lizardfolk":
+        changeAncestryTable(obj, index);
+        ability_flaw_boost(['str', 'wis', 'int']);
+        break;
+    case "Select a class":
+        document.getElementById("stat_table_hp").innerHTML = "";
+        document.getElementById("stat_table_size").innerHTML = "";
+        document.getElementById("stat_table_speed").innerHTML = "";
+        document.getElementById("stat_table_ab").innerHTML = "";
+        document.getElementById("stat_table_af").innerHTML = "";
+        break;
+    }
 };
