3246f5acd3ab67a382849495131f209b6bb82db8
Simbas Desktop <rech16@student.aau.dk>
2020-03-30 21:27:09 +0200

    added some more to background

--- app/static/js/background.js
+++ app/static/js/background.js
@@ -1,7 +1,18 @@
+const ability_full = ['Strength', 'Dexterity', 'Constitution', 'Intelligence', 'Wisdom', 'Charisma']
+const ability_short = ['str', 'dex', 'con', 'int', 'wis','cha'];
+
+
+
 function changeBackgroundTable(obj, index) {
     document.getElementById("background_table_desc").innerHTML = obj[index].backgroundText;
-    document.getElementById("background_table_ab_boosts").innerHTML = obj[index].ability;
+    document.getElementById("background_table_ab_boosts").insertAdjacentHTML("afterBegin", obj[index].ability[0]);
+    document.getElementById("background_table_ab_boosts1").insertAdjacentHTML("afterBegin", obj[index].ability[1]);
     document.getElementById("background_table_source").innerHTML = obj[index].source;
+    for (i = 0; i < 6; i++) {
+        if(obj[index].ability[0].includes(ability_full[i])){
+            document.getElementById('background_boost_select').insertAdjacentHTML("beforeEnd", '<option class="dropdown-item">'.concat(ability_full[i].concat('</option>')));
+        }
+    }
 };
 
 function resetBackgroundRowAbilityScores() {
@@ -12,3 +23,30 @@ function resetBackgroundRowAbilityScores() {
     document.getElementById("wis_background_mod").innerHTML = 0;
     document.getElementById("cha_background_mod").innerHTML = 0;
 };
+
+
+function background_boost(array){
+    resetAncestryRowAbilityScores();
+    for (i = 0; i < 2; i++) {
+        document.getElementById(array[i].concat('_ances_mod')).innerHTML = 2;
+    }
+    for (i = 2; i < 3; i++) {
+        document.getElementById(array[i].concat('_ances_mod')).innerHTML = -2;
+    }
+};
+
+
+
+function switchBackground(sel_background, obj, index){
+    switch (sel_background) {
+      case "Acolyte":
+        changeBackgroundTable(obj, index);
+        break;
+      case "Select a background":
+        document.getElementById("background_table_desc").innerHTML = "";
+        document.getElementById("background_table_ab_boosts").innerHTML = "";
+        document.getElementById("background_table_source").innerHTML = "";
+        break;
+
+    }
+}
