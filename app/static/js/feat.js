8c1de3b2d7a7d9451e3f49123ddbe686aab03fe1
Simbas Desktop <rech16@student.aau.dk>
2020-04-25 12:08:27 +0200

    trying to get feats to work

--- app/static/js/feat.js
+++ app/static/js/feat.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $("#featInput").on("keyup", function () {
         var value = $(this).val().toLowerCase();
-        $("#spell_list div.card").filter(function () {
+        $("#accordion-baseFeats #accordion-archTypeFeats").filter(function () {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
